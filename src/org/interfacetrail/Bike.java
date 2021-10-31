76254209bbd47b689e1b66d532a61eb29ccdbbe7
praveenpugazh <praveenpugazh84@gmail.com>
2021-10-31 09:01:39 +0530

    updated

--- src/org/interfacetrail/Bike.java
+++ src/org/interfacetrail/Bike.java
@@ -1,7 +1,7 @@
 package org.interfacetrail;
 
 public interface Bike {
-
+//interface is a blueprint of class consist of only umimplemented methods without method body
 	void brand();
 	void model();
 	void year();
